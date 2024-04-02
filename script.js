bd6a2f7ac77ede03cb385a6431bd971c236c3ebc
Malveena06 <108987525+Malveena06@users.noreply.github.com>
2024-04-02 14:03:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username === "" || password === "") {
+        alert("Please enter both username and password.");
+    } else {
+        alert("Login successful!");
+    }
+}
